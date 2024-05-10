53e2062e9e64edf3b164fce77b4317f10703d37a
AntonioJimenez2 <157631103+AntonioJimenez2@users.noreply.github.com>
2024-05-09 18:14:00 -0600

    update js/main.js modificado para mejorar modularizacion

--- js/main.js
+++ js/main.js
@@ -1,56 +1,22 @@
 import { conexionAPI } from "./conexionAPI.js";
-import { validarCampos } from "./validarCampos.js";
+import { crearCard } from "./crearCard.js";
 
-
+//direccion del servidor
 let urlApi = "http://localhost:3001/productos/";
 let urlApiBorrar = "http://localhost:3001/productos/";
 
 //constantes
-
 const contenedorProductos = document.querySelector(".contenedor-productos");
-const parrafoEnviar = document.querySelector(".parrafo-enviar");
-const botonLimpiar = document.querySelector(".input-limpiar");
-const formulario = document.querySelector("[data-formulario]");
-const parrafoNombre = document.querySelector(".parrafo-nombre");
-const parrafoImagen = document.querySelector(".parrafo-imagen");
-const parrafoPrecio = document.querySelector(".parrafo-precio");
 
-
-//seleccionar servidor local u online
-
+//seleccionar entre servidor local u online
 let activarServerOnline = confirm("¿Desea usar el servidor online?");
 if (activarServerOnline) {
   urlApi = "https://alurageek-c323e-default-rtdb.firebaseio.com/productos.json";
-  urlApiBorrar = "https://alurageek-c323e-default-rtdb.firebaseio.com/productos/";
+  urlApiBorrar =
+    "https://alurageek-c323e-default-rtdb.firebaseio.com/productos/";
 }
-
-botonLimpiar.addEventListener("click", () => {
-  parrafoEnviar.style.display = "none";
-  parrafoNombre.innerText=""
-  parrafoImagen.innerText=""
-  parrafoPrecio.innerText=""
-});
-
-
-//crear tarjeta de producto
-
-function crearCard(nombre, imagen, precio, id) {
-  const cardProducto = `<div class="card-producto" data-index="${id}">
-            <figure>
-              <div class="contenedor-imagen"><img class="imagen-producto" src="${imagen}" alt="${nombre}"/></div>
-              <figcaption class="nombre-producto">${nombre}</figcaption>
-            </figure>
-            <span class="contenedor-precio">
-              <p>$ ${precio}</p>
-              <img class="trash-icon" src="./imagenes/trash.png"/>
-            </span>
-          </div>`;
-  return cardProducto;
-}
-
 
 //mostrar los productos en la página
-
 mostrarProductos();
 async function mostrarProductos() {
   try {
@@ -95,52 +61,7 @@ async function mostrarProductos() {
   }
 }
 
-
-//validar campos
-formulario.addEventListener("submit", evento => {
-  evento.preventDefault();
-  const nombre = document.querySelector("[data-nombre-producto]").value;
-  const imagen = document.querySelector("[data-imagen-producto]").value;
-  const precio = document.querySelector("[data-precio-producto]").value;
-  if(validarCampos.validarNombre(nombre, imagen, precio)){
-    agregarProducto(nombre, imagen, precio)
-  }
-  });
-
-//agregar producto
-
-async function agregarProducto(nombre, imagen, precio) {
-  try {
-    const nuevoProducto = await conexionAPI.enviarProducto(
-      nombre,
-      imagen,
-      precio,
-      urlApi
-    );
-    if (nuevoProducto === null) {
-      throw new Error("Ocurrió un error al intentar agregar el producto");
-    }
-    console.log(`producto agregado: ${nuevoProducto}`);
-    //mensaje agregado en parrafo
-    parrafoEnviar.innerText = "Producto agregado con éxito";
-    parrafoEnviar.style.color = "green";
-    parrafoEnviar.style.display = "block";
-    //vaciar contenedor y volverlo a cargar con el producto agregado
-    contenedorProductos.innerHTML = "";
-    mostrarProductos();
-    return;
-  } catch (error) {
-    console.log(`Error al agregar el producto: ${error}`);
-    parrafoEnviar.innerText = "Error al intentar agregar el producto";
-    parrafoEnviar.style.color = "red";
-    parrafoEnviar.style.display = "block";
-  }
-}
-
-
-
 // eliminar tarjeta de producto
-
 contenedorProductos.addEventListener("click", evento =>
   eliminarProductos(evento)
 );
@@ -174,4 +95,6 @@ async function eliminarProductos(evento) {
   }
 }
 
-
+export { urlApi };
+export { mostrarProductos };
+export { contenedorProductos };
