290b6ac11daeda6ff9c3d6d92f3e1abf84bedf65
AntonioJimenez2 <157631103+AntonioJimenez2@users.noreply.github.com>
2024-05-09 18:11:41 -0600

    cambios para mejorar la modularizacion

--- js/validarCampos.js
+++ js/validarCampos.js
@@ -1,10 +1,33 @@
-//funcion validar datos del producto
+import { conexionAPI } from "./conexionAPI.js";
+import { urlApi } from "./main.js";
+import { mostrarProductos } from "./main.js";
+import { contenedorProductos } from "./main.js";
 
-function validarNombre(nombre, imagen, precio) {
+//constantes
+const parrafoEnviar = document.querySelector(".parrafo-enviar");
+const botonLimpiar = document.querySelector(".input-limpiar");
+const formulario = document.querySelector("[data-formulario]");
+const parrafoNombre = document.querySelector(".parrafo-nombre");
+const parrafoImagen = document.querySelector(".parrafo-imagen");
+const parrafoPrecio = document.querySelector(".parrafo-precio");
+
+//validar campos
+formulario.addEventListener("submit", evento => {
+  evento.preventDefault();
+  const nombre = document.querySelector("[data-nombre-producto]").value;
+  const imagen = document.querySelector("[data-imagen-producto]").value;
+  const precio = document.querySelector("[data-precio-producto]").value;
+  if (validarDatos(nombre, imagen, precio)) {
+    agregarProducto(nombre, imagen, precio);
+  }
+});
+
+//funcion validar datos del producto
+function validarDatos(nombre, imagen, precio) {
   const parrafoNombre = document.querySelector(".parrafo-nombre");
   const parrafoImagen = document.querySelector(".parrafo-imagen");
   const parrafoPrecio = document.querySelector(".parrafo-precio");
-  const regexNombre = /^[a-zA-Z\s']+$/;
+  const regexNombre = /^[a-zA-Z0-9.\s-]+$/;
   const regexImagen = /\.(jpeg|jpg|png|bmp|svg)$/i;
   const regexPrecio = /^\d+(\.\d{2})?$/;
 
@@ -32,6 +55,40 @@ function validarNombre(nombre, imagen, precio) {
   return nombreValido && imagenValida && precioValido;
 }
 
-export const validarCampos = {
-  validarNombre
-};
+//agregar producto
+async function agregarProducto(nombre, imagen, precio) {
+  try {
+    const nuevoProducto = await conexionAPI.enviarProducto(
+      nombre,
+      imagen,
+      precio,
+      urlApi
+    );
+    if (nuevoProducto === null) {
+      throw new Error("Ocurrió un error al intentar agregar el producto");
+    }
+    console.log(`producto agregado: ${nuevoProducto}`);
+    //mensaje agregado en parrafo
+    parrafoEnviar.innerText = "Producto agregado con éxito";
+    parrafoEnviar.style.color = "green";
+    parrafoEnviar.style.display = "block";
+    //vaciar contenedor y volverlo a cargar con el producto agregado
+    contenedorProductos.innerHTML = "";
+    mostrarProductos();
+    return;
+  } catch (error) {
+    console.log(`Error al agregar el producto: ${error}`);
+    parrafoEnviar.innerText = "Error al intentar agregar el producto";
+    parrafoEnviar.style.color = "red";
+    parrafoEnviar.style.display = "block";
+  }
+}
+
+//limpiar los parrafos
+botonLimpiar.addEventListener("click", () => {
+  parrafoEnviar.style.display = "none";
+  parrafoNombre.innerText = "";
+  parrafoImagen.innerText = "";
+  parrafoPrecio.innerText = "";
+});
+
